a2c7ff95b57d1f27fdc9ff7f7a7be1cf816c4a10
interpretor2021 <79582236+interpretor2021@users.noreply.github.com>
2021-02-25 21:55:49 +0530

    Delete src/main directory

--- src/main/java/com/db/interpretor/service/viewTextService.java
+++
@@ -1,5 +0,0 @@
-package com.db.interpretor.service;
-
-public interface viewTextService {
-	public String extractText(String appmyCredentials,String appprojectId,String appbucketName, String fileName);
-}
