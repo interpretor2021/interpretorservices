fefc2d39fc73797017c0d279e100836508375a8a
interpretor2021 <79582236+interpretor2021@users.noreply.github.com>
2021-02-26 13:23:07 +0530

    Delete src/main directory

--- src/main/java/com/db/interpretor/service/SearchTextService.java
+++
@@ -1,9 +0,0 @@
-package com.db.interpretor.service;
-
-import java.util.ArrayList;
-
-import com.db.interpretor.entity.FileDetails;
-
-public interface SearchTextService {
-		public ArrayList<FileDetails> searchText(String appmyCredentials,String appprojectId,String appbucketName, String appobjectName, String searchStr,String fileType);
-}
